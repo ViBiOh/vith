16096cbd3888e69f943b66718270d0876ce3bf04
Vincent Boutour <bob@vibioh.fr>
2023-09-02 09:45:59 +0200

    fix: Add signal to wait for termination
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/vith/api.go
+++ cmd/vith/api.go
@@ -9,6 +9,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"syscall"
 	"time"
 
 	_ "net/http/pprof"
@@ -108,6 +109,6 @@ func main() {
 
 	go appServer.Start(endCtx, "http", httputils.Handler(vithService.Handler(), healthService, recoverer.Middleware, telemetryService.Middleware("http")))
 
-	healthService.WaitForTermination(appServer.Done())
+	healthService.WaitForTermination(appServer.Done(), syscall.SIGTERM)
 	server.GracefulWait(appServer.Done(), vithService.Done(), streamHandlerService.Done(), thumbnailHandlerService.Done())
 }
